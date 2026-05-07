ccfa93a84d914445ad358332665a2bf9de7214a2
Shubham Yadav <shubhamyadav11122000@gmail.com>
2026-05-07 20:55:27 +0530

    Create Element Project

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 let btn = document.querySelector("button");
 let main = document.querySelector("main");
 
-btn.addEventListener("click", function () {
-  let div = document.createElement("div");
+let quotes = [
+  "Small steps every day.",
+  "Dream big, start small.",
+  "Be better than yesterday.",
+  "Stay humble, work hard.",
+  "Good things take time.",
+  "Never stop learning.",
+  "Believe in yourself.",
+  "Progress, not perfection.",
+  "Your time will come.",
+  "Make yourself proud.",
+];
 
-  let randomNum1 = Math.random() * 100;
-  let randomNum2 = Math.random() * 100;
+btn.addEventListener("click", function () {
+  let h1 = document.createElement("h1");
+
+  let randomNum1 = Math.random() * 80;
+  let randomNum2 = Math.random() * 80;
+  let randomNum3 = Math.floor(Math.random() * quotes.length);
+  let scale = Math.random() * 2 + 0.5;
+
+  let randomColour1 = Math.floor(Math.random() * 256);
+  let randomColour2 = Math.floor(Math.random() * 256);
+  let randomColour3 = Math.floor(Math.random() * 256);
+
   let rotation = Math.random() * 360;
 
-  let color1 = Math.floor(Math.random() * 256);
-  let color2 = Math.floor(Math.random() * 256);
-  let color3 = Math.floor(Math.random() * 256);
+  h1.innerText = quotes[randomNum3];
 
-  div.style.height = "50px";
-  div.style.width = "50px";
-  div.style.position = "absolute";
+  h1.style.position = "absolute";
 
+  h1.style.left = randomNum1 + "%";
+  h1.style.top = randomNum2 + "%";
+  h1.style.rotate = rotation + "deg";
+  h1.style.scale = scale;
+  h1.style.color = `rgb(${randomColour1},${randomColour2}, ${randomColour3})`;
 
-  div.style.left = randomNum1 + "%";
-  div.style.top = randomNum2 + "%";
-  div.style.rotate = rotation+'deg'
-
-  div.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-
-  main.appendChild(div);
-
-  console.log(div);
+  main.appendChild(h1);
 });

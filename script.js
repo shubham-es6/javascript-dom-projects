04511ebdefce9266c2b9b914b33377f9071ada02
Shubham Yadav <shubhamyadav11122000@gmail.com>
2026-05-08 20:09:35 +0530

    Create responsive download progress UI

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
+let grow = 0;
 let btn = document.querySelector("button");
-let main = document.querySelector("main");
-
-let quotes = [
-  "Small steps every day.",
-  "Dream big, start small.",
-  "Be better than yesterday.",
-  "Stay humble, work hard.",
-  "Good things take time.",
-  "Never stop learning.",
-  "Believe in yourself.",
-  "Progress, not perfection.",
-  "Your time will come.",
-  "Make yourself proud.",
-];
+let h2 = document.querySelector("h2");
+let inner = document.querySelector(".inner");
 
 btn.addEventListener("click", function () {
-  let h1 = document.createElement("h1");
+  btn.style.pointerEvents = "none";
 
-  let randomNum1 = Math.random() * 80;
-  let randomNum2 = Math.random() * 80;
-  let randomNum3 = Math.floor(Math.random() * quotes.length);
-  let scale = Math.random() * 2 + 0.5;
+  let num = 50 + Math.floor(Math.random() * 50);
 
-  let randomColour1 = Math.floor(Math.random() * 256);
-  let randomColour2 = Math.floor(Math.random() * 256);
-  let randomColour3 = Math.floor(Math.random() * 256);
-
-  let rotation = Math.random() * 360;
-
-  h1.innerText = quotes[randomNum3];
-
-  h1.style.position = "absolute";
-
-  h1.style.left = randomNum1 + "%";
-  h1.style.top = randomNum2 + "%";
-  h1.style.rotate = rotation + "deg";
-  h1.style.scale = scale;
-  h1.style.color = `rgb(${randomColour1},${randomColour2}, ${randomColour3})`;
-
-  main.appendChild(h1);
+  let int = setInterval(() => {
+    grow++;
+    h2.innerHTML = grow + "%";
+    inner.style.width = grow + "%";
+  }, num);
+  setTimeout(() => {
+    clearInterval(int);
+    btn.innerHTML = "Downloaded";
+    btn.style.opacity = 0.5;
+  }, num * 100);
 });

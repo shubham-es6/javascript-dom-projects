a7d8fc19c14d30c649854c69af95139d48659b1a
Shubham Yadav <shubhamyadav11122000@gmail.com>
2026-05-13 20:23:50 +0530

    Profile Card Generator

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-let btn = document.querySelector("#btn");
-let fileinp = document.querySelector("#fileinp");
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main = document.querySelector("main");
+let cards = document.querySelector(".cards");
 
-btn.addEventListener("click", function () {
-  fileinp.click();
-});
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
 
-fileinp.addEventListener("change", function (dets) {
-  const file = dets.target.files[0];
-  if (file) {
-    btn.textContent = file.name;
-  }
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let profile = document.createElement("div");
+  profile.classList.add("profile");
+
+  let img = document.createElement("img");
+  img.setAttribute("src", inputs[0].value);
+
+  let h3 = document.createElement("h3");
+  h3.textContent = inputs[1].value;
+
+  let h5 = document.createElement("h5");
+  h5.textContent = inputs[2].value;
+
+  let p = document.createElement("p");
+  p.textContent = inputs[3].value;
+
+  profile.appendChild(img);
+  card.appendChild(profile);
+  card.appendChild(h3);
+  card.appendChild(h5);
+  card.appendChild(p);
+
+  cards.appendChild(card);
+
+  inputs.forEach(function (inp) {
+    if (inp.type !== "submit") {
+      inp.value = "";
+    }
+  });
 });
